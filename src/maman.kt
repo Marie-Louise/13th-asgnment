7375c42a7e55ef2fb5f8724fe7cfbb2d3c98b70f
Marie-Louise <mariemuteteyimana@gmail.com>
2021-03-10 15:22:57 +0300

    13th assignment

---
+++ src/maman.kt
@@ -0,0 +1,31 @@
+    fun main() {
+    name()
+    modulus()
+        sum()
+personality()
+
+
+
+
+
+}
+fun name(){
+    println("hello marie")
+}
+fun modulus(){
+    var num1=20
+    var num2=3
+    var modulus=num1%num2
+    println(modulus)
+}
+    fun sum(){
+        var num1=6
+        var num2=8
+        var num3=4
+        var num4=2
+        var sum=num1+num2+num3+num4
+        println(sum)
+    }
+    fun personality(){
+        println("self confidence")
+    }
